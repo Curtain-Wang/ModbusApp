98e171d94fa1246d032b985f26b7ef36349e0ab6
Curtain-Wang <wangqt1997@qq.com>
2026-04-17 14:37:40 +0800

    去掉Fusion样式，避免Windows深色主题看不清字的bug

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QApplication::setStyle(QStyleFactory::create("Fusion")); // 或 "Windows"，避免原生 macOS 样式
+    // QApplication::setStyle(QStyleFactory::create("Fusion")); // 或 "Windows"，避免原生 macOS 样式
     w.show();
     return a.exec();
 }
